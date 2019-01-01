379cbd48d743d8f5d475a056d063e4a26ad4e276
Josue Peralta <jperal77@gmail.com>
2019-01-01 16:19:14 -0600

    fixed objectfit polyfill setup and made Layout into stateful component

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { StaticQuery, graphql } from 'gatsby'
 import Header from './header'
@@ -9,26 +9,41 @@ import 'bootstrap/dist/css/bootstrap-grid.min.css'
 import 'typeface-charm'
 import 'typeface-overlock'
 
-const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
+class Layout extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      year: new Date().getFullYear(),
+    }
+  }
+
+  componentDidMount() {
+    objectFitImages()
+  }
+
+  render() {
+    return (
+      <StaticQuery
+        query={graphql`
+          query SiteTitleQuery {
+            site {
+              siteMetadata {
+                title
+              }
+            }
           }
-        }
-      }
-    `}
-    render={data => (
-      <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        {children}
-        <Footer year="2018" />
-      </>
-    )}
-  />
-)
+        `}
+        render={data => (
+          <>
+            <Header siteTitle={data.site.siteMetadata.title} />
+            {this.props.children}
+            <Footer year={this.state.getFullYear} />
+          </>
+        )}
+      />
+    )
+  }
+}
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
